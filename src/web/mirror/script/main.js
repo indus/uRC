994e11aa2d85105d211ae39ee0ca0cf459be6a16
Stefan Keim <Stefan.Keim@dlr.de>
2014-07-21 17:46:10 +0200

    trigger

--- src/web/mirror/script/main.js
+++ src/web/mirror/script/main.js
@@ -48,44 +48,4 @@
     })
     .controller('mainCTRL', function ($scope) {
 
-        function mul2(args) {
-            var x = args[0];
-            var y = args[1];
-            console.log("mul2() called with " + x + " and " + y);
-            return x * y;
-        }
-
-        $scope.$on('connected', function () {
-            console.log('event received');
-            $scope.session.subscribe('mirror.reflection', function (args, kwargs) { console.log(kwargs); })
-                .then(
-                function (sub) {
-                    console.log('subscribed to topic');
-                },
-                function (err) {
-                    console.log('failed to subscribe to topic', err);
-                });
-        })
-
-
-
-
-        /*
-        session.register('com.example.mul3', mul2).then(
-            function (reg) {
-                console.log('procedure registered');
-            },
-            function (err) {
-                console.log('failed to register procedure', err);
-            });
-
-
-        session.subscribe('mirror.reflection', function (args, kwargs) { console.log(kwargs); }).then(
-           function (sub) {
-               console.log('subscribed to topic');
-           },
-           function (err) {
-               console.log('failed to subscribe to topic', err);
-           }
-        );*/
     });
